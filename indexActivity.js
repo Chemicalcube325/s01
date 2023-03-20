3b31643afb66ec67da47765c0d941415449bea87
Chemicalcube325 <JacobGutierrez325@gmail.com>
2023-03-20 15:53:03 +0800

    Add Activity Code

---
+++ indexActivity.js
@@ -0,0 +1,21 @@
+console.log("Hello, Welcome to De La Salle University!");
+
+let firstName = "Jacob";
+let lastName = "Gutierrez";
+let age = 21;
+let hobbies = ['Watching Anime', 'Reading Manga', 'Playing Video Games', 'Listening to Lofi', 'Reading Light Novels / Visual Novels'];
+let homeAddress = {
+	houseNumber: "Blk 3 lot 25 Phase 1",
+	street: "Barcelona",
+	subdivision: "Sta. Catalina",
+	barangay: "Salawag",
+	city: "Dasmarinas",
+	province: "Cavite",
+	zipCode: 4114
+};
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(hobbies);
+console.log(homeAddress);
